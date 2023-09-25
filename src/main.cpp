d11b0438c900fe3c5ceda67e5a0f20cf24c9b898
rodrigo <rodrigovbartholo@gmail.com>
2023-09-25 11:46:39 -0300

    Refactor code for better understanding

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,25 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// declarations
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
-}
-
 int main() {
-
+  // glfw: initialize and configure
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // glfw window creation
   GLFWwindow *window =
       glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGl", NULL, NULL);
+
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
@@ -34,11 +30,12 @@ int main() {
   glfwMakeContextCurrent(window);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // glad: load all OpenGl function pointers
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-
+  // render loop
   while (!glfwWindowShouldClose(window)) {
 
     // input
@@ -52,7 +49,19 @@ int main() {
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
-
+  // glfw: terminate, clearing all previously allocated GLFW resources
   glfwTerminate();
   return 0;
+}
+
+// process all input: query glfw whether relevant keys are pressed/released this
+// frame and react accordingly
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+// glfw: whenever the window size is changed, this callback function executes
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
